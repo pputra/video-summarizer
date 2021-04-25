7cb3e9d2674415142da0a216ffa2b7667816b8e4
pputra <mynameisyoga@live.com>
2021-04-25 13:34:15 -0700

    feat: detect boundaries using ssim (still temporary thresholds)

--- src/ShotBoundaryAnalysis.py
+++ src/ShotBoundaryAnalysis.py
@@ -1,21 +1,31 @@
 from skimage.metrics import structural_similarity as ssim
-import numpy as np
+
 import cv2
+import sys
 
-def mse(imageA, imageB):
-    err= np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-    err /= float(imageA.shape[0] * imageA.shape[1])
-    return err
+NUM_TOTAL_FRAMES = 16200
 
 
-def compare_image(imageA, imageB):
-    m = mse(imageA, imageB)
+def compare_frames(curr_frame, next_frame):
+    s = ssim(curr_frame, next_frame, multichannel=True)
+    return s
 
-    s = ssim(imageA, imageB, multichannel=True)
-    print(m)
-    print(s)
 
-frame1 = cv2.imread("project_dataset/frames/concert/frame2867.jpg")
-frame2 = cv2.imread("project_dataset/frames/concert/frame2869.jpg")
+if __name__ == '__main__':
+    frames_path = sys.argv[1]
 
-compare_image(frame1, frame2)
+    start_frame = 0
+
+    for i in range(NUM_TOTAL_FRAMES - 1):
+        curr_frame = cv2.imread(frames_path + "frame" + str(i) + ".jpg")
+        next_frame = cv2.imread(frames_path + "frame" + str(i + 1) + ".jpg")
+
+        s = compare_frames(curr_frame, next_frame)
+
+        # print(str(i) + " " + str(s))
+
+        shot_len = i - start_frame
+
+        if s < 0.25 and shot_len > 5:
+            print(str(start_frame) + " " + str(i))
+            start_frame = i + 1
