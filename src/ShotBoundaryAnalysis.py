d5b84acad99d15a3a51ab752aef1c53fcd6db483
pputra <mynameisyoga@live.com>
2021-04-25 16:04:48 -0700

    feat: improve ssim thresholds

--- src/ShotBoundaryAnalysis.py
+++ src/ShotBoundaryAnalysis.py
@@ -1,15 +1,13 @@
 from skimage.metrics import structural_similarity as ssim
-
 import cv2
+
 import sys
 
 NUM_TOTAL_FRAMES = 16200
 
-
 def compare_frames(curr_frame, next_frame):
     s = ssim(curr_frame, next_frame, multichannel=True)
     return s
-
 
 if __name__ == '__main__':
     frames_path = sys.argv[1]
@@ -22,10 +20,18 @@ if __name__ == '__main__':
 
         s = compare_frames(curr_frame, next_frame)
 
-        # print(str(i) + " " + str(s))
-
         shot_len = i - start_frame
 
-        if s < 0.25 and shot_len > 5:
+        concert_thresholds = s < 0.2 and shot_len > 5
+        meridian_thresholds = s < 0.3 and shot_len > 5
+        soccer_thresholds = s < 0.4 and shot_len > 5
+
+        is_last_frame = i == NUM_TOTAL_FRAMES - 2
+
+        if meridian_thresholds or is_last_frame:
+            if is_last_frame:
+                i = i + 1
+
             print(str(start_frame) + " " + str(i))
+
             start_frame = i + 1
